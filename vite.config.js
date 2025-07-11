e66d653ea9c07407a5b8064d232791f118cfed0a
v-s-vish03 <148931448+v-s-vish03@users.noreply.github.com>
2025-07-11 10:53:48 +0530

    Fix script path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/coffee-kiosk-sim/',  // ✅ required for GitHub Pages
+  base: '/coffee-kiosk-sim/', // REQUIRED for GitHub Pages
   test: {
     environment: 'jsdom',
     globals: true
