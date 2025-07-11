aa912e1bfd028f2f900b6df08697fccfdea775ef
v-s-vish03 <148931448+v-s-vish03@users.noreply.github.com>
2025-07-11 10:02:05 +0530

    CI/CD Pipeline

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/coffee-kiosk-sim/', // Replace with your repo name
+  base: '/coffee-kiosk-sim/', // GitHub Pages base path
+
+  test: {
+    environment: 'jsdom', // Enables browser-like test behavior
+    globals: true          // Enables `describe`, `it`, `expect`, `vi` without importing
+  }
 });
